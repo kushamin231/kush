510ceb0fdb01bb67f6012274a27e50802add0054
kushamin231 <144840109+kushamin231@users.noreply.github.com>
2023-11-24 23:34:54 -0500

    Commit-3
    Declared an array of image filenames and Loop through the image files array and create thumbnail images dynamically.

--- Assignment-4/Part-2/main.js
+++ Assignment-4/Part-2/main.js
@@ -1,18 +1,21 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+/* Declare an array of image filenames */
+const imageFiles = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-/* Declaring the alternative text for each image file */
+/* Loop through images */
+imageFiles.forEach((filename) => {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/' + filename);
+  newImage.setAttribute('alt', 'Image ' + filename);
+  thumbBar.appendChild(newImage);
 
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+  /* Adding click event listener to each thumbnail image */
+  newImage.addEventListener('click', function() {
+    displayedImage.setAttribute('src', 'images/' + filename);
+    displayedImage.setAttribute('alt', 'Image ' + filename);
+  });
+});
